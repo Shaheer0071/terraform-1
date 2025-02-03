06a87b4673fe9c31091e0946c1c37bd06cb38b6b
shaheer0071 <shaikshaheer007@gmail.com>
2025-02-03 17:49:33 +0530

    terraform

--- dynamic-blocks/ec2.tf
+++ dynamic-blocks/ec2.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "allow_tls" {
 dynamic "ingress" {
 
  for_each = [22,8080,3306]
- iterator = "port"
+ iterator = port
  content {
    from_port       = port.value
     to_port         = port.value
